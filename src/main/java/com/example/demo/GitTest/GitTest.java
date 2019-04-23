b3f4f9a9d9dd05cd88fdd7f3f5c1eb3080cd44ea
zhaiYong <l>
2019-04-23 11:18:33 +0800

    11111

--- src/main/java/com/example/demo/GitTest/GitTest.java
+++ src/main/java/com/example/demo/GitTest/GitTest.java
@@ -2,6 +2,8 @@ package com.example.demo.GitTest;
 
 public class GitTest {
     public static void main(String[] args) {
+        Integer i=1;
+        System.out.println(i);
         System.out.println("111");
     }
 }
